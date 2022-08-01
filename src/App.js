1d74cfe5ffffe43b5cb3a127c23c0feb55324690
elerizz <eleonorarizzitello@gmail.com>
2022-08-01 16:48:57 +0200

    feat: app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Generator from "./ components/Generator"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Generator/> 
+    </div>
+  );
+}
+
+export default App;
